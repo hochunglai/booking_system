abc7794f8d9f323d5e505581b53433e42de98b91
hochunglai <hochunglai@gmail.com>
2023-03-03 11:25:50 -0500

    working - test on Sunday

--- booking-system.py
+++ booking-system.py
@@ -1,41 +1,40 @@
+#!/usr/bin/env python3
+
 import threading
 import time
-
+import datetime
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 phone_numbers = [6133142900, 6138826678]
 emails = ["hochunglai@gmail.com", "chanyo99@hotmail.com"]
 names = ["Deryck Ho", "Yoyo Chan"]
+sessions = ["8:00", "9:15"]
 
 
 def start_threads():
+
     threads = []
-    start_time = time.time()
     for i in range(2):
-        thread = threading.Thread(target=book, args=(
-            i, phone_numbers[i], emails[i], names[i]))
-        threads.append(thread)
-        thread.start()
+        for j in range(2):
+            thread = threading.Thread(target=book, args=(
+                i, full_time_format(sessions[j]), phone_numbers[i], emails[i], names[i]))
+            threads.append(thread)
+            thread.start()
 
     # Wait for all threads to finish
     for thread in threads:
         thread.join()
 
-    end_time = time.time()
-    print(f"Time taken: {end_time - start_time} seconds")
 
-
-def book(num, phone_num, email, name):
+def book(num, session, phone_num, email, name):
 
     print(f"Worker {num} {phone_num} {email} {name} started")
 
     chrome_options = Options()
-
+    # chrome_options.add_argument("--headless")
     chrome_options.add_experimental_option("detach", True)
 
     driver = webdriver.Chrome(
@@ -43,17 +42,17 @@ def book(num, phone_num, email, name):
 
     driver.get("https://reservation.frontdesksuite.ca/rcfs/nepeansportsplex/Home/Index?Culture=en&PageId=b0d362a1-ba36-42ae-b1e0-feefaf43fe4c&ShouldStartReserveTimeFlow=False&ButtonId=00000000-0000-0000-0000-000000000000")
 
-    sport = driver.find_element_by_xpath(
-        "//div[@class='content' and text()='Squash - court 2']")
+    sport = driver.find_element(By.XPATH,
+                                "//div[@class='content' and text()='Badminton']")
     sport.click()
 
     try:
         # Number of People
-        num_people_input = driver.find_element_by_name("ReservationCount")
+        num_people_input = driver.find_element(By.NAME, "ReservationCount")
         num_people_input.clear()
         num_people_input.send_keys("2")
 
-        confirm_button = driver.find_element_by_id("submit-btn")
+        confirm_button = driver.find_element(By.ID, "submit-btn")
         confirm_button.click()
     except:
         print("\n\n\n\n\nNo need to enter number of people\n\n\n\n\n")
@@ -61,8 +60,8 @@ def book(num, phone_num, email, name):
     # Expend Date List
     while True:
         try:
-            date_button = driver.find_element_by_xpath(
-                '//*[@id="mainForm"]/div[2]/div[2]/a')
+            date_button = driver.find_element(By.XPATH,
+                                              f"//*[contains(text(), '{time_format()}')]")
             date_button.click()
         except:
             driver.refresh()
@@ -70,35 +69,45 @@ def book(num, phone_num, email, name):
         break
 
     # Choose time
-    elements = driver.find_elements(By.XPATH, f"//a[@aria-label]")
-    for element in elements:
-        try:
-            element.click()
-        except:
-            print(f"{num} {element.text} Failed")
-            continue
+    driver.find_element(By.XPATH, f"//a[@aria-label='{session}']").click()
 
     # Phone number
-    phone_number_input = driver.find_element_by_name("PhoneNumber")
-    phone_number_input.send_keys(phone_num)
+    driver.find_element(By.ID, 'telephone').send_keys(phone_num)
 
     # Email
-    email_input = driver.find_element_by_name("Email")
-    email_input.send_keys(email)
+    driver.find_element(By.NAME, 'Email').send_keys(email)
 
     # Name
-    name_input = driver.find_element_by_xpath('//input[@type="text"]')
-    name_input.send_keys(name)
+    driver.find_element(By.XPATH, '//input[@type="text"]').send_keys(name)
 
-    confirm_button = driver.find_element_by_id("submit-btn")
-    # confirm_button.click()
+    driver.find_element(By.ID, "submit-btn").click()
 
-    confirm_button = driver.find_element_by_id("submit-btn")
-    # confirm_button.click()
+    driver.find_element(By.ID, "submit-btn").click()
 
+    input()
     driver.quit()
 
-    print(f"Worker {num} finished")
+
+def full_time_format(session):
+    today = datetime.date.today()
+
+    # Add 2 days to today's date
+    dt = today + datetime.timedelta(days=2)
+
+    formatted_date = session + dt.strftime(" %p %A %B %-d, %Y")
+    print("\n\n"+formatted_date+"\n\n")
+    return formatted_date
+
+
+def time_format():
+    today = datetime.date.today()
+
+    # Add 2 days to today's date
+    dt = today + datetime.timedelta(days=2)
+
+    formatted_date = dt.strftime("%A %B %-d, %Y")
+    print("\n\n"+formatted_date+"\n\n")
+    return formatted_date
 
 
 start_threads()
